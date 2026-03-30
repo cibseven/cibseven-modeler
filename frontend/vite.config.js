17529b68899f1d47db783d4d9f826b44562acce5
Adrián Alejo Trujillo <adrian90.at@gmail.com>
2026-03-30 12:11:43 +0100

    chore: import cib-common-frontend library (#36)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -146,7 +146,7 @@ export default defineConfig({
           fileName: (format) => `cibseven-modeler.${format}.js`,
         },
         rollupOptions: {
-          external: ['vue', /^\/assets\/images\//, 'bootstrap', 'vue-i18n', 'vue-router', 'axios', 'vuex', 'bpmn-js', 'dmn-js', '@bpmn-io/form-js'],
+          external: ['vue', /^\/assets\/images\//, '@cib/bootstrap-components', 'bootstrap', 'vue-i18n', 'vue-router', 'axios', 'vuex', 'bpmn-js', 'dmn-js', '@bpmn-io/form-js'],
           output: {
             globals: {
               vue: 'Vue',
