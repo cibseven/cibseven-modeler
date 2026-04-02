2e962ce5cffa17a43017e27c22524b03ddb3b61e
Miguel Dourado <migueldo@cib.de>
2026-04-02 15:55:14 +0100

    refactor: improved code

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -298,4 +298,10 @@ export const addHtmlErrorsToConsole = error => {
 	return error
 }
 
+export const formatFileSize = (bytes) => {
+	if (bytes < 1024) return `${bytes} B`
+	if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`
+	return `${(bytes / (1024 * 1024)).toFixed(1)} MB`
+}
+
 export { TYPEC7, TYPEDMN }
