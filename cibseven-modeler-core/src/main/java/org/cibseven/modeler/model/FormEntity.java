4cab05f5d01e8c27c3a45992a6e2f6a8beac95b9
Serge Krot <serge.krot@cib.de>
2026-04-09 14:30:50 +0300

    chore(pom) remove unused dependencies

--- cibseven-modeler-core/src/main/java/org/cibseven/modeler/model/FormEntity.java
+++ cibseven-modeler-core/src/main/java/org/cibseven/modeler/model/FormEntity.java
@@ -26,7 +26,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 
 import lombok.Getter;
